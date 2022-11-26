9757dda006e00b575e50bed48f5dcea22367ab41
Adrian6303 <104848915+Adrian6303@users.noreply.github.com>
2022-11-26 14:13:06 +0200

    Update lab_service.py

--- service/lab_service.py
+++ service/lab_service.py
@@ -1,215 +1,235 @@
 from domain.entities import Student
 from domain.entities import PbLaborator
-from domain.validators import Validator
-from repository.lab_repo import InMemoryRepository
 
 
-class StudentService:
-    def __init__(self, repo, validator):
+class Console:
+    def __init__(self, srvL, srvS, srvR):
         """
-        Initializeaza service
-        :param repo: obiect de tip repo care ne ajuta sa gestionam multimea de seriale
-        :type repo: InMemoryRepository
-        :param validator: validator pentru verificarea serialelor
-        :type validator: ShowValidator
+        Initializeaza consola
+        :type srvL: LabService
+        :type srvS: StudentService
         """
-        self.__repo = repo
-        self.__validator = validator
+        self.__srvL = srvL
+        self.__srvS = srvS
+        self.__srvR = srvR
 
-    def add_student(self, studentID, nume, grupa):
+    def __print_all(self):
+
+        student_list = self.__srvS.get_all_students()
+        if len(student_list) == 0:
+            print('Nu exista studenti in lista.')
+        else:
+            print('Lista de studenti este:')
+            for student in student_list:
+                # print(student)
+                print('Studentul ' + str(student.getNume()) + ' (ID:' + str(
+                    student.getStudentID()) + ') din grupa: ' + str(student.getGrup()))
+
+        probleme_list = self.__srvL.get_all_problems()
+        if len(probleme_list) == 0:
+            print('Nu exista probleme in lista.')
+        else:
+            print('Lista de probleme este:')
+            for problema in probleme_list:
+                # print(student)
+                print('Problema nr ' + str(problema.getNrLab_nrPb()) + ': ' + str(
+                    problema.getDescriere()) + ', termen limita ' + str(problema.getDeadline()))
+
+    def __add_student(self):
         """
-        Adauga student
-        :param studentID: Id-ul studentului
-        :type studentID:int
-        :param nume: numele studentului
-        :type nume:str
-        :param grupa:numarul grupei de care apartine
-        :type grupa:int
-        :return: obiectul de tip Student creat
-        :rtype:-; studentul s-a adaugat in lista
-        :raises: ValueError daca studentul are date invalide
+        Adauga un serial cu datele citite de la tastatura
         """
-        s = Student(studentID, nume, grupa)
+        nume = str(input("Numele Studentului:"))
 
-        self.__validator.validate_student(s)
-        self.__repo.store_student(s)
-        return s
+        try:
+            id = int(input("ID-ul studentului:"))
+            grupa = int(input("Numarul grupei:"))
+        except ValueError:
+            print('Numarul grupei si id-ul trebuie sa fie un numar.')
+            return
 
-    def get_all_students(self):
+        try:
+            added_student = self.__srvS.add_student(id, nume, grupa)
+            print('Studentul ' + added_student.getNume() + ' (ID:' + str(
+                added_student.getStudentID()) + ') a fost adaugat cu succes.')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __add_pbLab(self):
+        nrLab_nrPb = str(input("Numarul laboratorului si problemei:"))
+
+        descriere = str(input("Descrierea problemei:"))
+        deadline = str(input("Termenul limita:"))
+
+        try:
+            added_pbLab = self.__srvL.add_pbLab(nrLab_nrPb, descriere, deadline)
+            print('Problema ' + str(added_pbLab.getNrLab_nrPb()) + ' : ' + str(
+                added_pbLab.getDescriere()) + '; Termen limita: ' + str(
+                added_pbLab.getDeadline()), '; a fost adaugata cu succes')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __delete_student(self):
+        id = int(input("ID-ul studentului:"))
+        try:
+            deleted_student = self.__srvS.delete_student(id)
+            print('Studentul ' + deleted_student.getNume() + ' din grupa ' + str(
+                deleted_student.getGrup()) + ' a fost sters cu succes (IDStudent=' + str(
+                deleted_student.getStudentID()) + ').')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __delete_pbLab(self):
+        nr = str(input("Nr lab si pb:"))
+        try:
+            deleted_pbLab = self.__srvL.delete_pbLab(nr)
+            print('Problema nr ' + deleted_pbLab.getNrLab_nrPb() + ' cu termen ' + str(
+                deleted_pbLab.getDeadline()) + ' a fost sters cu succes')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __edit_student(self):
+        id = int(input('ID-ul studentului:'))
+        nume = input("Numele studentului:")
+
+        try:
+            grupa = int(input("Grupa din care face parte studentul:"))
+        except ValueError:
+            print('Grupa trebuie sa fie un numar.')
+            return
+
+        try:
+            modified_student = self.__srvS.edit_student(id, nume, grupa)
+            print('Studentul ' + modified_student.getNume() + ' (ID: ' + str(
+                modified_student.getStudentID()) + ') a fost modificat cu succes.')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __edit_pbLab(self):
+        nr = str(input('Nr lab si pb:'))
+        descriere = str(input("Descrierea problemei:"))
+        deadline = str(input("Termenul limita:"))
+
+        try:
+            modified_problem = self.__srvL.edit_pbLab(nr, descriere, deadline)
+            print('Problema ' + modified_problem.getNrLab_nrPb() + ' cu termen limita ' + str(
+                modified_problem.getDeadline()) + ' a fost modificat cu succes.')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __search_student(self):
+        id = int(input('ID-ul studentului:'))
+
+        try:
+            searched_student = self.__srvS.search_student(id)
+            print('Studentul ' + searched_student.getNume() + ' (ID: ' + str(
+                searched_student.getStudentID()) + ') din grupa ' + str(
+                searched_student.getGrup()))
+        except ValueError as ve:
+            print(str(ve))
+
+    def __search_pbLab(self):
+        nr = input('Nr-ul problemei:')
+
+        try:
+            searched_problem = self.__srvL.search_pbLab(nr)
+            print('Problema nr' + searched_problem.getNrLab_nrPb() + ' cu termen limita  ' + str(
+                searched_problem.getDeadline()) + ': ' + str(searched_problem.getDescriere()))
+        except ValueError as ve:
+            print(str(ve))
+
+    def __random_student(self):
         """
-        Returneaza o lista cu toati studenti disponibili
-        :return: lista de studenti disponibili
-        :rtype: list of objects de tip Student
+        Genereaza un student random
         """
-        return self.__repo.get_all_students()
+        nr = int(input("Numar studenti random:"))
 
-    def delete_student(self, id):
+        try:
+            for i in range(nr):
+                random_student = self.__srvR.random_student()
+                print('Studentul ' + random_student.getNume() + ' (ID:' + str(
+                    random_student.getStudentID()) + ') a fost adaugat cu succes.')
+        except ValueError as ve:
+            print(str(ve))
+
+    def __random_pbLab(self):
         """
-        Sterge serialul cu id dat din lista
-        :param id: id-ul dat
-        :type id: int
-        :return: studentul sters
-        :rtype: Serial
-        :raises: ValueError daca nu exista serial cu id-ul dat
+        Genereaza un problema random
         """
-        self.__validator.validate_StudentID(id)
-        return self.__repo.delete_student(id)
+        nr = int(input("Numar probleme random:"))
 
-    def edit_student(self, id, nume, grupa):
-        """
-                Modifica datele studentului cu id dat
-                :param id: id-ul serialului de modificat
-                :type id: int
-                :param nume: noul titlu al serialului
-                :type nume: str
-                :param grupa: noul an de aparitie al serialului
-                :type grupa: int
-                :return: studentul modificat
-                :rtype:Student
-                :raises: ValueError daca noile date nu sunt valide, sau nu exista student cu id dat
-                """
-        s = Student(id, nume,grupa)
+        try:
+            for i in range(nr):
+                random_problem = self.__srvR.random_pbLab()
+                print('Problema nr ' + random_problem.getNrLab_nrPb() + ' cu termen limita ' + str(
+                    random_problem.getDeadline()) + ' : ' + str(
+                    random_problem.getDescriere()))
+        except ValueError as ve:
+            print(str(ve))
 
-        self.__validator.validate_student(s)
-        return self.__repo.edit_student(id, s)
-
-    def search_student(self, id):
-        """
-            Cauta studentul cu id-ul dat
-            :param id: id-ul studentului cautat
-            :type id: int
-            :return: studentul cautat
-            :rtype:Student
-            :raises: ValueError daca noile date nu sunt valide, sau nu exista student cu id dat
-        """
-        self.__validator.validate_StudentID(id)
-        return self.__repo.search_student(id)
-
-
-
-class LabService:
-    """
-        GRASP Controller (Curs 6)
-        Responsabil de efectuarea operatiilor cerute de utilizator
-        Coordoneaza operatiile necesare pentru a realiza actiunea declansata de utilizator
-        (i.e. declansare actiune: utilizator -> ui-> obiect tip service in ui -> service -> service coordoneaza operatiile
-        folosind alte obiecte (e.g. repo, validator) pentru a realiza efectiv operatia)
-        """
-
-    def __init__(self, repo, validator):
-        """
-        Initializeaza service
-        :param repo: obiect de tip repo care ne ajuta sa gestionam multimea de seriale
-        :type repo: InMemoryRepository
-        :param validator: validator pentru verificarea serialelor
-        :type validator: ShowValidator
-        """
-        self.__repo = repo
-        self.__validator = validator
-
-
-    def add_pbLab(self, nrLab_nrPb, descriere, deadline):
-        """
-        Adauga pbLab
-        :param nrLab_nrPb: nr lab si nr pb
-        :type nrLab_nrPb:str
-        :param descriere: descrierea problemei
-        :type descriere:str
-        :param deadline:termenul de predare a problemei
-        :type deadline:str
-        :return: obiectul de tip PbLaborator creat
-        :rtype:-; problema s-a adaugat in lista
-        :raises: ValueError daca problema are date invalide
-        """
-        p = PbLaborator(nrLab_nrPb, descriere, deadline)
-
-        self.__validator.validate_pbLab(p)
-        self.__repo.store_pbLab(p)
-        return p
-
-    def get_all_problems(self):
-        """
-        Returneaza o lista cu toate problemele disponibile
-        :return: lista de probleme disponibile
-        :rtype: list of objects de tip PbLaborator
-        """
-        return self.__repo.get_all_problems()
-
-    def delete_pbLab(self, nr):
-        """
-        Sterge serialul cu id dat din lista
-        :param nr: numarul dat
-        :type nr: str
-        :return: problema stearsa
-        :rtype: PbLaborator
-        :raises: ValueError daca nu exista problema cu nr dat
-        """
-        self.__validator.validate_NrLab_nrPb(nr)
-        return self.__repo.delete_pbLab(nr)
-
-    def edit_pbLab(self, nr, descriere, deadline):
-        """
-                        Modifica datele problemei cu id dat
-                        :param nr: nr problemei de modificat
-                        :type nr: str
-                        :param descriere: noua descriere a problemei
-                        :type descriere: str
-                        :param deadline: noul termen limita a problemei
-                        :type deadline: int
-                        :return: problema modificata
-                        :rtype:PbLaborator
-                        :raises: ValueError daca noile date nu sunt valide, sau nu exista problema cu nr dat
-                        """
-        pb = PbLaborator(nr, descriere, deadline)
-
-        self.__validator.validate_pbLab(pb)
-        return self.__repo.edit_pbLab(nr, pb)
-
-    def search_pbLab(self, nr):
-        """
-            Cauta problema cu nr-ul dat
-            :param nr: nr-ul problemei
-            :type nr: str
-            :return: problema cautata
-            :rtype: PbLaborator
-            :raises: ValueError daca noile date nu sunt valide, sau nu exista problema cu nr dat
-        """
-        self.__validator.validate_NrLab_nrPb(nr)
-        return self.__repo.search_pbLab(nr)
-
-
-def test_add_student():
-    repo = InMemoryRepository()
-    validator = Validator()
-    test_srv = StudentService(repo, validator)
-
-    added_student = test_srv.add_student(545677, 'Tractoreanu Leonardo', 12)
-    assert (added_student.getStudentID() == 545677)
-    assert (added_student.getNume() == 'Tractoreanu Leonardo')
-    assert (added_student.getGrup() == 12)
-
-    assert (len(test_srv.get_all_students()) == 1)
-
-    try:
-        added_student = test_srv.add_student(545, 'Tractoreanu Leonardo', 12)
-        assert False
-    except ValueError:
-        assert True
-
-def test_add_probleme():
-    repo = InMemoryRepository()
-    validator = Validator()
-    test_srv = LabService(repo, validator)
-
-    added_problem = test_srv.add_pbLab('1_5', 'Sirul lui Fibbonaci', '10 octombrie')
-    assert (added_problem. getNrLab_nrPb() == '1_5')
-    assert (added_problem.getDescriere() == 'Sirul lui Fibbonaci')
-    assert (added_problem.getDeadline() == '10 octombrie')
-
-    assert (len(test_srv.get_all_problems()) == 1)
-
-    try:
-        added_problem = test_srv.add_pbLab('1231233_51231', 'Sirul lui Fibbonaci', '10 octombrie')
-        assert False
-    except ValueError:
-        assert True
+    def gestiune_lab_ui(self):
+        # command-driven menu (just to have something different)
+        # Lab7-9 oricare varianta (print-menu + optiuni/comenzi) este ok
+        # dar si la comenzi sa existe un user guide, ce comenzi sunt dispobibile, cum le folosim
+        # if using Python 3.10 and bored with if statements,
+        # you can try: https://learnpython.com/blog/python-match-case-statement/
+        while True:
+            print('Comenzi disponibile: add, delete, edit, search, asign lab, grade lab, stats, random, show all, exit')
+            cmd = input('Comanda este:')
+            cmd = cmd.lower().strip()
+            if cmd == 'add':
+                print('Selecteaza: student, pb_lab')
+                cmd = input('Comanda este:')
+                if cmd == 'student':
+                    self.__add_student()
+                elif cmd == 'pb_lab':
+                    self.__add_pbLab()
+                else:
+                    print('Comanda invalida.')
+            elif cmd == 'delete':
+                print('Selecteaza: student, pb_lab')
+                cmd = input('Comanda este:')
+                if cmd == 'student':
+                    self.__delete_student()
+                elif cmd == 'pb_lab':
+                    self.__delete_pbLab()
+                else:
+                    print('Comanda invalida.')
+            elif cmd == 'edit':
+                print('Selecteaza: student, pb_lab')
+                cmd = input('Comanda este:')
+                if cmd == 'student':
+                    self.__edit_student()
+                elif cmd == 'pb_lab':
+                    self.__edit_pbLab()
+                else:
+                    print('Comanda invalida.')
+            elif cmd == 'search':
+                print('Selecteaza: student, pb_lab')
+                cmd = input('Comanda este:')
+                if cmd == 'student':
+                    self.__search_student()
+                elif cmd == 'pb_lab':
+                    self.__search_pbLab()
+                else:
+                    print('Comanda invalida.')
+            elif cmd == 'random':
+                print('Selecteaza: student, pb_lab')
+                cmd = input('Comanda este:')
+                if cmd == 'student':
+                    self.__random_student()
+                elif cmd == 'pb_lab':
+                    self.__random_pbLab()
+                else:
+                    print('Comanda invalida.')
+            elif cmd == 'show all':
+                self.__print_all()
+            elif cmd == 'exit':
+                # this should not be here per principles of app organization
+                # added just to showcase how the static method works
+                print("Total number of student objects created (including tests):", Student.getNumberOfStudentObjects())
+                print("Total number of PbLaborator objects created (including tests):",
+                      PbLaborator.getNumberOfProblemObjects())
+                return
+            else:
+                print('Comanda invalida.')
